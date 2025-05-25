5f7ee3be56837915d039d2a43ec66660edd4c507
meshjohn <meshjohn1@gmail.com>
2025-05-25 17:21:32 +0300

    update audience

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,9 +1,9 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-    nce: "https://full-stack-real-estate-eight.vercel.app",
-    issuerBaseURL: "https://dev-sbpud2ugulilvnq0.us.auth0.com",
-    tokenSigningAlg: "RS256"
+  audience: "https://full-stack-real-estate-eight.vercel.app",
+  issuerBaseURL: "https://dev-sbpud2ugulilvnq0.us.auth0.com",
+  tokenSigningAlg: "RS256",
 });
 
 export default jwtCheck;
