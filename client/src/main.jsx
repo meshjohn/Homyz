12b3056fbc4f24f2ec5c4bc02f3de0542be7e4ae
meshjohn <meshjohn1@gmail.com>
2025-05-25 17:04:59 +0300

    update auth0 links

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider domain="dev-sbpud2ugulilvnq0.us.auth0.com" clientId="NfgsTyQQUKUeLvGtSEdRJKrSR4xbecb3" authorizationParams={{
+    <Auth0Provider domain="dev-sbpud2ugulilvnq0.us.auth0.com" clientId="hpInVKaHrVDyPdHAnXsfkMQ5MR0OBy5K" authorizationParams={{
       redirect_uri: "https://homyz-mu.vercel.app"
     }}
     audience="https://full-stack-real-estate-eight.vercel.app"
