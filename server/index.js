8436a31759327532063c317bd4831ac6e6757d18
meshjohn <meshjohn1@gmail.com>
2025-05-25 17:17:01 +0300

    update cors

--- server/index.js
+++ server/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {userRoute} from "../server/routes/userRoute.js";
-import {residencyRoute} from "../server/routes/residencyRoute.js";
+import { userRoute } from "../server/routes/userRoute.js";
+import { residencyRoute } from "../server/routes/residencyRoute.js";
 
 dotenv.config();
 
@@ -13,12 +13,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://homyz-mu.vercel.app", // allow your frontend domain
+    credentials: true, // optional, if you're using cookies or Auth0
+  })
+);
 
 app.use("/api/user", userRoute);
 app.use("/api/residency", residencyRoute);
 
 app.listen(port, () => {
-    console.log(`port ${port}`);
+  console.log(`port ${port}`);
 });
-
