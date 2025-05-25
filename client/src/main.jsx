4c465c44d8d1df4603c1f6f016e1fd8a0991a701
meshjohn <meshjohn1@gmail.com>
2025-05-25 18:03:50 +0300

    update audience

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider domain="dev-sbpud2ugulilvnq0.us.auth0.com" clientId="hpInVKaHrVDyPdHAnXsfkMQ5MR0OBy5K" authorizationParams={{
-      redirect_uri: "https://homyz-mu.vercel.app"
+     redirect_uri: window.location.origin,
     }}
     audience="https://full-stack-real-estate-eight.vercel.app"
     scope="openid profile email"
